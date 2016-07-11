c567e8fd043526a0e696b4a47e1ec94dd2bac38b
王朋飞 <wazsj6457@foxmail.com>
2016-07-11 10:59:24 +0800

    添加创建 保存二维码功能

--- app/src/main/java/com/wpf/qrcodedemo/MainActivity.java
+++ app/src/main/java/com/wpf/qrcodedemo/MainActivity.java
@@ -1,27 +1,36 @@
 package com.wpf.qrcodedemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.wpf.qrcodescanview.CreateQRImage;
 import com.wpf.qrcodescanview.ScanQRCodeActivity;
+import com.wpf.requestpermission.RequestPermission;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private int requestCode = 100;
     private TextView textView;
+    private ImageView imageView;
     private Button button;
+    private RequestPermission requestPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +46,28 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == this.requestCode && resultCode == RESULT_OK) {
             String result = data.getStringExtra("ResultString");
             textView.setText(result);
-            Toast.makeText(this,result,Toast.LENGTH_LONG).show();
+            new CreateQRImage(result) {
+                @Override
+                public void onFinish(final Bitmap bitmap) {
+                    imageView.setImageBitmap(bitmap);
+                    requestPermission = new RequestPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE,1) {
+                        @Override
+                        public void onSuccess() {
+                            saveBitmapToSD(bitmap, Environment.getExternalStorageDirectory().getPath()+"/","1.jpg");
+                        }
+
+                        @Override
+                        public void onFail(String[] failList) {
+
+                        }
+                    };
+                }
+            };
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        requestPermission.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 }
---
+++ qrcodescanview/src/main/java/com/wpf/qrcodescanview/CreateQRImage.java
@@ -0,0 +1,75 @@
+package com.wpf.qrcodescanview;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Hashtable;
+
+public abstract class CreateQRImage extends AsyncTask<String,Integer,Bitmap> {
+
+	private String url;
+	private int QR_WIDTH = 512, QR_HEIGHT = 512;
+
+	private Bitmap createQRImage(String url, int QR_WIDTH, int QR_HEIGHT) {
+		Bitmap bitmap = null;
+		try {
+			if (url == null || "".equals(url) || url.length() < 1) return null;
+			Hashtable<EncodeHintType, String> hints = new Hashtable<>();
+			hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+			BitMatrix bitMatrix = new QRCodeWriter()
+					.encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
+			int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+			for (int y = 0; y < QR_HEIGHT; y++)
+				for (int x = 0; x < QR_WIDTH; x++)
+					pixels[y * QR_WIDTH + x] = bitMatrix.get(x, y)?0xff000000:0xffffffff;
+			bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
+			bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+		}
+		catch (WriterException e) {
+			e.printStackTrace();
+		}
+		return bitmap;
+	}
+
+	protected CreateQRImage(String url) {
+		this.execute(url);
+	}
+
+	protected CreateQRImage(String url,int QR_WIDTH,int QR_HEIGHT) {
+		this.url = url;
+		this.QR_WIDTH = QR_WIDTH;
+		this.QR_HEIGHT = QR_HEIGHT;
+		this.execute(url);
+	}
+
+	@Override
+	protected void onPostExecute(Bitmap bitmap) {
+		onFinish(bitmap);
+	}
+
+	@Override
+	protected Bitmap doInBackground(String... strings) {
+		return createQRImage(strings[0],QR_WIDTH,QR_HEIGHT);
+	}
+
+	public static boolean saveBitmapToSD(Bitmap bitmap, String filePath, String fileName) {
+		try {
+			FileOutputStream fos = new FileOutputStream(filePath + fileName);
+			bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
+			return true;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public abstract void onFinish(Bitmap bitmap);
+}
